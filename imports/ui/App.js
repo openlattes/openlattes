49e6994f0f3d1f95f9f4223fecea8e2e6ce63342
Murilo Dutra <murilodut@gmail.com>
2018-08-22 09:37:31 -0300

    Style change: move gql query out of the component

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
+const GET_HELLO = gql`
+  {
+    hello
+  }
+`;
+
 const App = () => (
-  <Query
-    query={gql`
-      {
-        hello
-      }
-    `}
-  >
+  <Query query={GET_HELLO} >
     {({ loading, error, data }) => {
       if (loading) return <p>loading...</p>;
       if (error) return <p>error</p>;
