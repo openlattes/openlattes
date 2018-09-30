e4c875b17a1ee5f0645671f593cee683e3056730
Murilo Dutra <murilodut@gmail.com>
2018-09-29 22:28:50 -0300

    Add CssBaseline

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Link,
 } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import Graph from './Graph';
 import ProductionIndicator from './ProductionIndicator';
 
 const App = () => (
-  <Router>
-    <div>
-      <ul>
-        <li><Link to="/coautorias">Coautorias</Link></li>
-        <li><Link to="/producoes">Produções</Link></li>
-      </ul>
-      <Route path="/coautorias" component={Graph} />
-      <Route path="/producoes" component={ProductionIndicator} />
-    </div>
-  </Router>
+  <Fragment>
+    <CssBaseline />
+    <Router>
+      <div>
+        <ul>
+          <li><Link to="/coautorias">Coautorias</Link></li>
+          <li><Link to="/producoes">Produções por ano</Link></li>
+        </ul>
+        <Route path="/coautorias" component={Graph} />
+        <Route path="/producoes" component={ProductionIndicator} />
+      </div>
+    </Router>
+  </Fragment>
 );
 
 export default App;
