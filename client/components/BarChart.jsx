a0e2b7b85e4f081e18f93250534c855c19248380
Murilo Dutra <murilodut@gmail.com>
2018-09-30 01:10:41 -0300

    Sort types indicator

--- client/components/BarChart.jsx
+++ client/components/BarChart.jsx
@@ -2,26 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { OrdinalFrame } from 'semiotic';
 
-const BarChart = ({ data }) => (
-  <OrdinalFrame
-    size={[1000, 500]}
-    data={data}
-    oAccessor="type"
-    rAccessor="count"
-    type="bar"
-    projection="horizontal"
-    axis={{
-      rotate: '45',
-    }}
-    margin={{
-      top: 5, bottom: 50, right: 20, left: 300,
-    }}
-    oLabel
-    sortO={(a, b) => a > b}
-    oPadding={2}
-    baseMarkProps={{ forceUpdate: true }}
-  />
-);
+const BarChart = ({ data }) => {
+  const indexedData = data
+    .reduce((obj, el) =>
+      Object.assign(obj, {
+        [el.type]: el.count,
+      }), {});
+
+  return (
+    <OrdinalFrame
+      size={[1000, 500]}
+      data={data}
+      oAccessor="type"
+      rAccessor="count"
+      type="bar"
+      projection="horizontal"
+      axis={{
+        rotate: '45',
+      }}
+      margin={{
+        top: 5, bottom: 50, right: 20, left: 300,
+      }}
+      oLabel
+      sortO={(a, b) => indexedData[a] > indexedData[b]}
+      oPadding={2}
+      baseMarkProps={{ forceUpdate: true }}
+    />
+  );
+};
 
 BarChart.propTypes = {
   data: PropTypes.shape({
