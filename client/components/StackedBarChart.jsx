bf35078e96778b45e5c821535a2970678b8a8666
Murilo Dutra <murilodut@gmail.com>
2018-10-01 15:23:59 -0300

    Add custom tooltip to stacked bar chart

--- client/components/StackedBarChart.jsx
+++ client/components/StackedBarChart.jsx
@@ -1,6 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { OrdinalFrame } from 'semiotic';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const customTooltipContent = (d) => {
+  const total = d.pieces.reduce((sum, { count }) => sum + count, 0);
+
+  return (
+    <Card
+      style={{
+        position: 'relative',
+        left: '-40px',
+        bottom: '78px',
+        zIndex: 999999,
+      }}
+      raised
+    >
+      <CardContent>
+        <Typography variant="body2">{total}</Typography>
+      </CardContent>
+    </Card>
+  );
+};
 
 const StackedBarChart = ({ data, colorHash }) => (
   <OrdinalFrame
@@ -23,6 +46,8 @@ const StackedBarChart = ({ data, colorHash }) => (
     sortO={(a, b) => a - b}
     oPadding={2}
     baseMarkProps={{ forceUpdate: true }}
+    hoverAnnotation
+    tooltipContent={customTooltipContent}
   />
 );
 
