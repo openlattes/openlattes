605b49653b106b35b14f0a9fa0fa3f77945a4077
Murilo Dutra <murilodut@gmail.com>
2018-10-06 16:15:38 -0300

    Update model Collaboration attributes

--- server/api/models/Collaboration.js
+++ server/api/models/Collaboration.js
@@ -5,6 +5,7 @@ const { Schema } = mongoose;
 const collaborationSchema = new Schema({
   members: [Schema.Types.ObjectId],
   productions: [Schema.Types.ObjectId],
+  supervisions: [Schema.Types.ObjectId],
 });
 
 export default mongoose.model('collaborations', collaborationSchema);
