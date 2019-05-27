df2dfcdceead0bd8adb7ef3a26ad5179a5ee6ba5
Murilo Dutra <murilodut@gmail.com>
2019-05-27 00:36:58 -0300

    Change customTooltipContent

--- client/components/StackedBarChart.jsx
+++ client/components/StackedBarChart.jsx
@@ -1,27 +1,31 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { OrdinalFrame } from 'semiotic';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
+import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
 const customTooltipContent = (d) => {
   const total = d.pieces.reduce((sum, { count }) => sum + count, 0);
 
   return (
-    <Card
+    <Paper
       style={{
         position: 'relative',
-        left: '-40px',
+        left: '-58px',
         bottom: '78px',
         zIndex: 999999,
+        padding: 5,
+        minWidth: 120,
       }}
-      raised
+      elevation={12}
     >
-      <CardContent>
-        <Typography variant="subtitle2">{total}</Typography>
-      </CardContent>
-    </Card>
+      <Typography variant="subtitle2" align="center">
+        {`Total: ${total}`}
+      </Typography>
+      <Typography variant="body2">
+        Clique para listar
+      </Typography>
+    </Paper>
   );
 };
 
