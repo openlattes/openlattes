5a94204b0d2c877e618eb525fa041fae40fcbc2c
Murilo Dutra <murilodut@gmail.com>
2019-05-27 02:42:08 -0300

    Change the panel header background color

--- client/components/CustomCard.jsx
+++ client/components/CustomCard.jsx
@@ -6,9 +6,9 @@ import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 
-const styles = theme => ({
+const styles = () => ({
   header: {
-    backgroundColor: theme.palette.primary.light,
+    backgroundColor: 'lightgray',
     padding: 5,
   },
   content: {
