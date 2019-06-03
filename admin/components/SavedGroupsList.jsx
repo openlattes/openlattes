9578446c066ec700fbb889dd1863e0e8434f79c4
Murilo Dutra <murilodut@gmail.com>
2019-06-03 15:28:22 -0300

    Delete groups from the client

--- admin/components/SavedGroupsList.jsx
+++ admin/components/SavedGroupsList.jsx
@@ -2,36 +2,51 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
 
 import SimpleTable from '../../client/components/SimpleTable';
 
+const DELETE_GROUP = gql`
+  mutation($group: String) {
+    deleteGroup(group: $group)
+  }
+`;
+
 const SavedSelectionsList = props => (
-  <SimpleTable
-    title="Grupos"
-    headers={[
-      {
-        id: 'name',
-        label: 'Nome',
-        align: 'left',
-        disablePadding: false,
-      },
-      {
-        id: 'delete',
-        label: 'Excluir',
-        align: 'right',
-        disablePadding: false,
-      },
-    ]}
-    data={props.groupNames.map(groupName => ({
-      id: groupName,
-      name: groupName,
-      delete: (
-        <IconButton aria-label="Delete" >
-          <DeleteIcon fontSize="small" />
-        </IconButton>
-      ),
-    }))}
-  />
+  <Mutation mutation={DELETE_GROUP}>
+    {deleteGroup => (
+      <SimpleTable
+        title="Grupos"
+        headers={[
+          {
+            id: 'name',
+            label: 'Nome',
+            align: 'left',
+            disablePadding: false,
+          },
+          {
+            id: 'delete',
+            label: 'Excluir',
+            align: 'right',
+            disablePadding: false,
+          },
+        ]}
+        data={props.groupNames.map(groupName => ({
+          id: groupName,
+          name: groupName,
+          delete: (
+            <IconButton
+              aria-label="Delete"
+              onClick={() => deleteGroup({ variables: { group: groupName } })}
+            >
+              <DeleteIcon fontSize="small" />
+            </IconButton>
+          ),
+        }))}
+      />
+    )}
+  </Mutation>
 );
 
 SavedSelectionsList.propTypes = {
